8f4d7c1fda7ff43c5fc8ecc2bc1e4d2d6998529a
datashaman <marlinf@datashaman.com>
2018-10-19 10:12:53 +0200

    wip

--- app.py
+++ app.py
@@ -1,103 +1,33 @@
-import calendar
-import dateutil.parser
-import gipc
-import hashlib
-import hmac
-import json
-import logging
+import robocop
 import os
-import requests
-import sys
-import time
 
 from flask import Flask, abort, request
 from slackclient import SlackClient
-from wit import Wit
 
-module = sys.modules[__name__]
 slack = SlackClient(os.environ['SLACK_API_TOKEN'])
-wit = Wit(os.environ['WIT_TOKEN'])
 
 app = Flask(__name__)
-app.logger.level = logging.DEBUG
-app.logger.debug('PID: %s', os.getpid())
 
-bot_id = slack.api_call('auth.test')['user_id']
+@robocop.on('/robocop')
+def command(form):
+    return 'Your wish is my command'
 
-def dispatch(data, spawn=True):
-    function = data['type']
-    if hasattr(module, function):
-        if spawn:
-            gipc.start_process(getattr(module, function), (data,))
-            return 'OK'
-        else:
-            return getattr(module, function)(data)
-    else:
-        app.logger.warn('%s not handled', function)
-        abort(400)
-
-def valid_request():
-    timestamp = request.headers['X-Slack-Request-Timestamp']
-    if abs(time.time() - float(timestamp)) > 60 * 5:
-        return False
-    key = bytes(os.environ['SLACK_SIGNING_SECRET'], 'utf-8')
-    msg = ('v0:' + timestamp + ':' + request.data.decode()).encode('utf-8')
-    signature = 'v0=' + hmac.new(key, msg, hashlib.sha256).hexdigest()
-    return hmac.compare_digest(signature, request.headers['X-Slack-Signature'])
-
-def strip_bot_id(text):
-    return text.replace('<@%s>' % bot_id, '')
-
-# Callback handlers
-def event_callback(payload):
-    event = payload['event']
-    if event['type'] == 'message' and event.get('subtype') == 'bot_message':
-        return 'OK'
-    return dispatch(event)
-
-def url_verification(payload):
-    return payload['challenge']
-
-# Event handlers
-def app_mention(event):
-    app.logger.debug('PID: %s', os.getpid())
-    wit_response = wit.message(strip_bot_id(event['text']))
-
-    if wit_response['entities']['intent'][0]['value'] == 'weather':
-        location = wit_response['entities']['location'][0]
-        coords = location['resolved']['values'][0]['coords']
-        url = 'https://api.darksky.net/forecast/%s/%s,%s' % (os.environ['DARK_SKY_KEY'],
-                                                             coords['lat'],
-                                                             coords['long'])
-        if 'datetime' in wit_response['entities']:
-            when = dateutil.parser.parse(wit_response['entities']['datetime'][0]['value'])
-            timestamp = calendar.timegm(when.utctimetuple())
-            url += ',%s' % timestamp
-
-        response = requests.get(url)
-        weather = response.json()
-        app.logger.debug(json.dumps(weather, indent=4))
+@robocop.on('app_mention')
+def echo(event):
+    if event.get('subtype') != 'bot_message':
         slack.api_call(
             'chat.postMessage',
             channel=event['channel'],
-            text=weather['currently']['summary'],
+            text=event['text']
         )
-
-    return 'OK'
-
-def message(event):
-    slack.api_call(
-        'chat.postMessage',
-        channel=event['channel'],
-        text=event['text']
-    )
-    return 'OK'
 
 @app.route('/', methods=['POST'])
 def home():
-    if valid_request():
-        payload = request.get_json()
-        app.logger.debug('payload %s', payload)
-        spawn = payload['type'] not in ['event_callback', 'url_verification']
-        return dispatch(payload, spawn)
-    abort(400)
+    response = robocop.handle(request)
+
+    if response is True:
+        return ''
+    elif response is False:
+        abort(400)
+
+    return response
---
+++ robocop.py
@@ -0,0 +1,58 @@
+import gipc
+import hashlib
+import hmac
+import os
+import time
+
+handlers = {}
+
+def on(name):
+    def decorator_on(func):
+        global handlers
+
+        if name[0] == '/':
+            handlers[name] = func
+        else:
+            if name not in handlers:
+                handlers[name] = []
+            handlers[name].append(func)
+
+        return func
+    return decorator_on
+
+def handle_command(form):
+    command = form.get('command')
+    if command in handlers:
+        return handlers[command](form)
+    return False
+
+def handle_event(event):
+    event_type = event['type']
+
+    if event_type in handlers:
+        for func in handlers[event_type]:
+            gipc.start_process(func, (event,))
+        return True
+    return False
+
+def valid_request(request):
+    timestamp = request.headers['X-Slack-Request-Timestamp']
+    if abs(time.time() - float(timestamp)) > 60 * 5:
+        return False
+    key = bytes(os.environ['SLACK_SIGNING_SECRET'], 'utf-8')
+    msg = ('v0:' + timestamp + ':' + request.get_data().decode()).encode('utf-8')
+    signature = 'v0=' + hmac.new(key, msg, hashlib.sha256).hexdigest()
+    return hmac.compare_digest(signature, request.headers['X-Slack-Signature'])
+
+def handle(request):
+    if valid_request(request):
+        if 'command' in request.form:
+            return handle_command(request.form)
+
+        payload = request.get_json()
+
+        if payload['type'] == 'event_callback':
+            return handle_event(payload['event'])
+        elif payload['type'] == 'url_verification':
+            return payload['challenge']
+    return False
