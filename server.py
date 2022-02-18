36cad1531b7bf7e2c50db196a2f44adcfdcedc98
timogoosen <timogoosen@gmail.com>
2022-02-18 11:01:52 +0200

    Try with original server.py but different uwsgi config

--- server.py
+++ server.py
@@ -1,7 +1,3 @@
-#from mrtallyman import create_app
-from mrtallyman import app
+from mrtallyman import create_app
 
-# app = create_app()
-
-if __name__ == "__main__":
-    app.run()
+app = create_app()
